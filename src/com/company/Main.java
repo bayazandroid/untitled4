6cd2de45ee61136390d2f0f7f70bd9532049cd9d
bayazandroid <bayaz.orozobekov@gmail.com>
2022-05-19 18:50:49 +0300

    set commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Hero[] heroes = {new Magic(), new Medic(), new Warrior()};
         for (int i = 0; i < heroes.length; i++) {
-            heroesAbility(heroes[i]);
+            System.out.println(new Magic().superAbility("magic"));
+            System.out.println(new Medic().superAbility("medic"));
+            System.out.println(new Warrior().superAbility("critical damage"));
+            System.out.println(new Medic().increaseExperience());
         }
-    }
-
-    public static void heroesAbility(Hero hero){
-        System.out.println(new Magic().superAbility("magic"));
-        System.out.println(new Medic().superAbility("medic"));
-        System.out.println(new Warrior().superAbility("critical damage"));
-        System.out.println(new Medic().increaseExperience());
     }
 }
